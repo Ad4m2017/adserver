e953ca41f6608ecb11da55ab7b0692814c8ac2ab
Ad4m2017 <d3geee@gmail.com>
2018-06-18 23:51:56 +0200

    update

--- index.php
+++ index.php
@@ -3,14 +3,49 @@
     <head>
 		<meta charset="UTF-8">
 		<title></title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" integrity="sha256-oSrCnRYXvHG31SBifqP2PM1uje7SJUyX0nTwO2RJV54=" crossorigin="anonymous" />
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<script	src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.qrcode/1.0/jquery.qrcode.min.js" integrity="sha256-9MzwK2kJKBmsJFdccXoIDDtsbWFh8bjYK/C7UjB1Ay0=" crossorigin="anonymous"></script>
 		<script	src="javascript.js"></script>
     </head>
-    <body>
-        <div id="main">
-        </div>
-    </body>
+<body>
+    <div id="main">
+   	  <h1>Create a new Advertisement</h1>
+      <div id="submit_box">
+
+          <div class="l title">Ad-Name</div>
+          <input class="r" id="ad-name" value="">
+
+          <div class="c"></div>
+
+          <div class="l title">Ad-Size</div>
+          <input class="r" id="ad-size" value="">
+
+          <div class="c"></div>
+
+          <div class="l title">Ad-Image</div>
+          <input class="r" id="ad-image" value="">
+
+          <div class="c"></div>
+
+          <div class="l title">Price</div>
+          <input class="r" id="ad-price" value="">
+
+          <div class="c"></div>
+
+          <div class="l title">Rules</div>
+          <input class="r" id="ad-rules" value="">
+
+          <div class="c"></div>
+
+          <div class="l title">Ad-Website</div>
+          <input class="r" id="ad-website" value="http://www.xyz.com">
+
+          <div class="c"></div>
+
+      </div>
+  </div>
+</body>
 </html>
 
