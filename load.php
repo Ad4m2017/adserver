52d94d0c81b7139a1cc246df789d5c567ad127f2
Ad4m2017 <d3geee@gmail.com>
2018-06-19 23:50:55 +0200

    update

--- load.php
+++ load.php
@@ -15,10 +15,26 @@ switch ($step) {
 		echo '</select>';
 		break;
 	case 2:
-		echo "i ist gleich 1";
+		echo '<div class="title">Select Ad Size:</div>';
+		echo '<select id="ad-size">';
+		echo '	<option value="" selected disabled></option>';
+		echo '	<option value="160x600">160x600</option>';
+		echo '	<option value="120x600">120x600</option>';
+		echo '	<option value="728x90">728x90</option>';
+		echo '</select>';
+		break;
 		break;
 	case 3:
-		echo "i ist gleich 2";
+		echo '<div class="title">Select Date(s):</div>';
+		echo '	<select name="dates" multiple>';
+		echo '		<option value="19.06.2018">19.06.2018 | 1 USD</option>';
+		echo '		<option value="20.06.2018" disabled>20.06.2018 | 1 USD</option>';
+		echo '		<option value="21.06.2018">21.06.2018 | 1 USD</option>';
+		echo '		<option value="22.06.2018">22.06.2018 | 1 USD</option>';
+		echo '		<option value="23.06.2018">23.06.2018 | 1 USD</option>';
+		echo '	</select>';
+		echo '<div class="txt-c">Price:<span id="price">0 USD</span></div>';
+		echo '<div class="txt-c"><div id="btn_save" class="green_button">Save</div></div>';
 		break;
 	default:
 		//echo "i ist nicht gleich 0, 1 oder 2";
